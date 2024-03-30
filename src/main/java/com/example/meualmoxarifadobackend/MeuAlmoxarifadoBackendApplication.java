177eab396fd2c983f9a5938f2d528263667b8362
devLucasMoraes <devlucasmoraes@gmail.com>
2024-03-30 13:33:37 -0300

    Initial commit

---
+++ src/main/java/com/example/meualmoxarifadobackend/MeuAlmoxarifadoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.meualmoxarifadobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuAlmoxarifadoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuAlmoxarifadoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meualmoxarifadobackend/MeuAlmoxarifadoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meualmoxarifadobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuAlmoxarifadoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
